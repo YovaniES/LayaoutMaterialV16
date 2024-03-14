ec1cc194348e5ef90269ebf6b4e809e6537ea5f9
YovaniES <yovany.21se@gmail.com>
2024-03-14 15:59:31 -0500

    DELETE TABLE 1

--- src/app/routes/tables/tables.module.ts
+++ src/app/routes/tables/tables.module.ts
@@ -2,12 +2,10 @@ import { NgModule } from '@angular/core';
 import { SharedModule } from '@shared/shared.module';
 import { TableRoutingModule } from './tables-routing.module';
 
-import { TablesKitchenSinkComponent } from './kitchen-sink/kitchen-sink.component';
-import { TablesKitchenSinkEditComponent } from './kitchen-sink/edit/edit.component';
 import { TablesRemoteDataComponent } from './remote-data/remote-data.component';
 
-const COMPONENTS: any[] = [TablesKitchenSinkComponent, TablesRemoteDataComponent];
-const COMPONENTS_DYNAMIC: any[] = [TablesKitchenSinkEditComponent];
+const COMPONENTS: any[] = [TablesRemoteDataComponent];
+const COMPONENTS_DYNAMIC: any[] = [];
 
 @NgModule({
   imports: [SharedModule, TableRoutingModule],
